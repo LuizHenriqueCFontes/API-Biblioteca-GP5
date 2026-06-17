296894c1ec622c9147a042d48eae7d21ce17cca5
Luiz <luizhenrique3781@gmail.com>
2026-06-16 22:04:18 -0300

    feat: initialize model reading

---
+++ src/main/java/com/biblioteca/gp5/reading/model/Reading.java
@@ -0,0 +1,14 @@
+package com.biblioteca.gp5.reading.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "reading")
+@Data
+public class Reading {
+	
+	
+
+}
