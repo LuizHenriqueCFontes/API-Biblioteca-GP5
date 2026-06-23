06d8d79b2ee9e7d6c6c0ccca1f5d445061369e80
Luiz <luiz@gmail.com>
2026-06-22 21:20:35 -0300

    feat: add file url mapper

--- src/main/java/com/biblioteca/gp5/loan/mapper/LoanMapper.java
+++ src/main/java/com/biblioteca/gp5/loan/mapper/LoanMapper.java
@@ -5,14 +5,15 @@ import org.mapstruct.Mapping;
 
 import com.biblioteca.gp5.loan.dto.response.BookLoanResponseDTO;
 import com.biblioteca.gp5.loan.model.Loan;
+import com.biblioteca.gp5.shared.mapper.FileUrlMapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = FileUrlMapper.class)
 public interface LoanMapper {
 	
 	@Mapping(source = "loan.book.title", target = "title")
-	@Mapping(source = "loan.book.coverUrl", target = "coverUrl")
+	@Mapping(source = "loan.book.coverUrl", target = "coverUrl", qualifiedByName = "toFileUrl")
 	@Mapping(source = "loan.book.authors", target = "authors")
-	@Mapping(source = "loan.book.fileUrl", target = "fileUrl")
+	@Mapping(source = "loan.book.fileUrl", target = "fileUrl", qualifiedByName = "toFileUrl")
 	@Mapping(source = "loan.book.idBook", target = "bookId")
 	public BookLoanResponseDTO toBookLoanResponseDTO(Loan loan, boolean hasReading);
 
